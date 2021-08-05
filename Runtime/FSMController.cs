4a2cb5f515a1d6b1ed46f883a895c3c21504028c
LittleShadeAzel <alexander.cvetanovic@bluewin.ch>
2021-08-05 22:34:18 +0200

    Initial commit

---
+++ Runtime/FSMController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LGP.FSM {
+    /// <summary>
+    /// Enabels the use of FSMStates inside a Behaviour.
+    /// </summary>
+    public class FSMController {
+
+        #region Variables
+        /// <summary>
+        /// The monoBehaviour for this FSMController. It's used for Coroutines.
+        /// </summary>
+        public MonoBehaviour monoBehaviour;
+        /// <summary>
+        /// The manifest of the FSMController's FSMStates.
+        /// </summary>
+        public Dictionary<int, FSMState> states = new Dictionary<int, FSMState>();
+        /// <summary>
+        /// If true, the FSMController can't change the active FSMState.
+        /// </summary>
+        public bool isStateLocked;
+        private int currentStateId = -1;
+        private int previousStateId = -1;
+		#endregion
+
+		#region Property
+        /// <summary>
+        /// Gets the active FSMState.
+        /// </summary>
+		public FSMState State { get => (currentStateId >= 0 ? states[currentStateId] : null); }
+
+        /// <summary>
+        /// Gets the id of the active FSMState.
+        /// </summary>
+        public int StateId { get => currentStateId; }
+		#endregion
+		
+        #region Constructor
+		public FSMController() { }
+        public FSMController(MonoBehaviour monoBehaviour, int initialState) {
+            this.monoBehaviour = monoBehaviour;
+            SetState(initialState);
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Adds a FSMState to the FSMController.
+        /// </summary>
+        public void AddState(int id, FSMState state) {
+            id = Mathf.Max(id, 0);
+            state.fsmController = this;
+            state.id = id;
+            states.Add(id, state);
+        }
+
+        /// <summary>
+        /// Adds a FSMState to the FSMController.
+        /// </summary>
+        public void AddState(int id, OnStateEnterCallback enter, OnStateTickCallback tick, OnStateExitCallback exit) {
+            AddState(id, new FSMState(enter, tick, exit));
+        }
+
+        /// <summary>
+        /// Switches to a diffrent FSMState.
+        /// </summary>
+        /// <param name="id">The id of the FSMState.</param>
+        /// <param name="forceSameState">Forces the FSMController to accept and procedure the same FSMState like normaly and doesnt ignore it.</param>
+        /// <param name="useReenter">Determines whether the FSMController should use "Reenter" Method instead of the regular "Enter" Method when using the same State.</param>
+        public virtual void SetState(int id, bool forceSameState = false, bool useReenter = false) {
+            FSMState newState = states[id];
+
+            // Handle state lock and empty state.
+            if (newState == null || isStateLocked) return;
+
+            // Handle same State.
+            if (id == currentStateId) {
+                if (!forceSameState) return;
+                if (useReenter) State.onStateReenter?.Invoke();
+            }
+
+            // Handle state Exiting.
+            if (State != null) {
+                State.SetTransitioning(true);
+                State.onStateExit?.Invoke();
+            }
+
+            // Handle State Entering.
+            previousStateId = currentStateId;
+            currentStateId = id;
+            if (id != currentStateId && !useReenter) State.onStateEnter?.Invoke();
+            State.SetTransitioning(false);
+        }
+
+        /// <summary>
+        /// Manually Updates the active FSMState for the regular Update Method.
+        /// </summary>
+        public void UpdateStateController() {
+            if (State == null) return;
+            if (State.onStateTick == null) return;
+            if (State.isFixedUpdate) return;
+            if (!State.IsTransitioning) SetState(State.onStateTick.Invoke());
+        }
+
+        /// <summary>
+        /// Manually Updates the active FSMState for the Fixed Update Method.
+        /// </summary>
+        public void UpdateStateControllerFixed() {
+            if (State == null) return;
+            if (State.onStateTick == null) return;
+            if (!State.isFixedUpdate) return;
+            if (!State.IsTransitioning) SetState(State.onStateTick.Invoke());
+        }
+        #endregion
+    }
+}
---
+++ Runtime/FSMState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LGP.FSM {
+
+    #region Delegates
+    public delegate void OnStateEnterCallback();
+    public delegate int OnStateTickCallback();
+    public delegate void OnStateExitCallback();
+    public delegate void OnStateReenterCallback();
+    #endregion
+
+    /// <summary>
+    /// A FSMState used by the FSMController.
+    /// </summary>
+    public class FSMState {
+        #region Variables
+        public int id;
+        public FSMController fsmController;
+        public OnStateEnterCallback onStateEnter;
+        public OnStateTickCallback onStateTick;
+        public OnStateExitCallback onStateExit;
+        public OnStateReenterCallback onStateReenter;
+        private bool isTransitioning;
+        /// <summary>
+        /// Determines whether or not the state is going to be updated in the Fixed Update or not.
+        /// </summary>
+        public bool isFixedUpdate = false;
+		#endregion
+
+		#region Properties
+        /// <summary>
+        /// It shows if the FSMState is transitioning into another FSMState. It's used by the FSMController to determine when a FSMState is done and shouldn't be updated anymore.
+        /// </summary>
+        public bool IsTransitioning { get => isTransitioning; }
+		#endregion
+
+		#region Contructors
+		public FSMState() { }
+        public FSMState(OnStateEnterCallback enter, OnStateTickCallback tick, OnStateExitCallback exit, OnStateReenterCallback reenter = null) {
+            onStateEnter = enter;
+            onStateTick = tick;
+            onStateExit = exit;
+            onStateReenter = reenter;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Sets the flag for the FSMState to end and transition into another one.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetTransitioning(bool value) {
+            isTransitioning = value;
+		}
+		#endregion
+	}
+}
+
